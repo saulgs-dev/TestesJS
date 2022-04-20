3682bffa886a19407bc9c829b85d1a8314815587
Saul Gomes <klowd.bk@gmail.com>
2022-04-19 21:14:46 -0300

    exercicio form

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-let nome = "Saul Gomes";
-function falaNome(){
-    return nome + "Bem vindo!";
+function meuEscopo(){
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+    
+    function recebeEventoForm(evento){
+        evento.preventDefault();
+
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+        
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        });
+        
+        console.log(pessoas);
+        
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}</p>`;
+    }
+    form.addEventListener('submit', recebeEventoForm);
 }
-falaNome(nome);
+meuEscopo();
